f76a274d7944991823c63d4c1f7095dc3a861f83
Sudheer <rskumar780@gmail.com>
2022-07-08 16:09:10 +0530

    Added react

---
+++ Frontend/myapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
